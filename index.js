63acde5343170dd97eea1b22c6f0a009623272d4
Sabrina <sabrinasrimulyani@gmail.com>
2022-05-31 16:36:19 +0700

    fix: add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const router = require('./routes');
 
 const PORT = 8080;
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', router);
 
 app.listen(PORT, () => console.log(`Server run on Port ${PORT}`));
